415325f9f031e89355edb7dfed92555ae75d364a
Chris D'Aloisio <chris@creativecontrol.com.au>
2018-12-05 23:19:20 +1100

    Refactor ready to do day2 part 2

--- 2018/day2/solution.hs
+++ 2018/day2/solution.hs
@@ -5,12 +5,22 @@ module Main where
 import           Data.List
 
 main :: IO ()
-main = print . reduce . map f . lines =<< readFile "input.txt"
+main = runSolution solution1
+
+solution2 :: [String] -> String
+solution2 list = "TODO"
+
+solution1 :: [String] -> Int
+solution1 = reduce . map f
   where
     f :: String -> (Int, Int)
     f str =
       let uniqNums = counts str
        in (existsToInt 2 uniqNums, existsToInt 3 uniqNums)
+
+-- Helpers
+runSolution :: (Show a) => ([String] -> a) -> IO ()
+runSolution solution = print . solution . lines =<< readFile "input.txt"
 
 sumTuples :: (Int, Int) -> (Int, Int) -> (Int, Int)
 sumTuples (a, b) (c, d) = (a + c, b + d)
